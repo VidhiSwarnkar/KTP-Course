3d264be0b69574af5c6453864bb14f7ace6b8ea8
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-04-19 21:35:03 +0530

    StageOfLifeMiniApp(::stageOfLife)

--- ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
+++ ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
@@ -1,7 +1,18 @@
 import com.faangx.ktp.basics.StageOfLifeMiniApp
 
 fun stageOfLife(age: Int): String {
-    TODO()
+    if (age < 3) {
+        return "Infancy"
+    } else if (age < 12) {
+        return "Childhood"
+    } else if (age < 18) {
+        return "Teenage"
+    } else if (age < 60) {
+        return "Adulthood"
+    } else {
+        return "Old age"
+
+    }
 }
 
 fun main() {
