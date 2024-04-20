e7a4e55434e8ced12ae1d5e96e770344e994a61b
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-04-20 18:52:08 +0530

    LeapYearCheckerMiniApp

--- ProgrammingFundamentals/Ep2/LeapYearChecker/src/Task.kt
+++ ProgrammingFundamentals/Ep2/LeapYearChecker/src/Task.kt
@@ -4,7 +4,8 @@ fun isLeapYear(year: Int): Boolean {
     if ((year % 4 ==0 && year % 100 != 0) || (year % 400 == 0)) {
         return true
     }  else {
-            return false
+          return false
+
     }
 }
 
