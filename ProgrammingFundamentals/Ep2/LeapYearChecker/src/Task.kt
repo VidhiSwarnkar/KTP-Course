e83119dbecf6ff8978410fc7b31ef0c8b0bcfb36
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-04-21 16:17:50 +0530

    LeapYearCheckerMiniApp

--- ProgrammingFundamentals/Ep2/LeapYearChecker/src/Task.kt
+++ ProgrammingFundamentals/Ep2/LeapYearChecker/src/Task.kt
@@ -2,10 +2,9 @@ import com.faangx.ktp.basics.LeapYearCheckerMiniApp
 
 fun isLeapYear(year: Int): Boolean {
     if ((year % 4 ==0 && year % 100 != 0) || (year % 400 == 0)) {
-        return true
+         return true
     }  else {
-          return false
-
+        return false
     }
 }
 
