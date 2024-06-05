012c8785b9b670fcb69fcf2be69dd7032d66f190
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-06-05 14:59:35 +0530

    solved LcmHcfCalculator.HCF

--- ProgrammingFundamentals/Ep4/LCM-HCF/src/Task.kt
+++ ProgrammingFundamentals/Ep4/LCM-HCF/src/Task.kt
@@ -1,11 +1,18 @@
 import com.faangx.ktp.basics.LcmHcfCalculatorMiniApp
 
 fun getLCM(x: Int, y: Int): Int {
-    TODO()
+    return 0
 }
 
 fun getHCF(x: Int, y: Int): Int {
-    TODO()
+    var a = x
+    var b = y
+    while ( b != 0) {
+        val rem = a%b
+        a = b
+        b = rem
+    }
+    return a
 }
 
 fun main() {
