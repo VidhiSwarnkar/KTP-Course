682c3d4e26477f88d2f01bbe44562048df98f4d0
VidhiSwarnkar <vidhiswarnkar@gmail.com>
2024-06-11 22:35:05 +0530

    solved OddEvenChecker

--- ProgrammingFundamentals/Ep2/OddEvenChecker/src/Task.kt
+++ ProgrammingFundamentals/Ep2/OddEvenChecker/src/Task.kt
@@ -1,13 +1,13 @@
 import com.faangx.ktp.basics.OddEvenCheckerMiniApp
 
 fun checkEvenOdd(num: Int): String {
-    if (num % 2 == 0) {
-        return "Even"
-    }   else  {
-        return "Odd"
-
-    }
+	if (num % 2 == 0) {
+		return "Even"
+	} else {
+		return "Odd"
+	}
 }
+
 
 fun main() {
     OddEvenCheckerMiniApp(::checkEvenOdd)
